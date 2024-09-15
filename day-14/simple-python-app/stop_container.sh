7ea2a3bb3363283e7250d5ec00abe48574ade928
anisharun2608 <54118712+anisharun2608@users.noreply.github.com>
2024-09-15 22:45:07 +0530

    adding stop execution in stop_container.sh

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+#To get the docker id of the existing running docker image
+container_id=$(sudo docker ps -q)
+
 # Stop the running container (if any)
-echo "Hi"
+docker rm -f ${container_id}
